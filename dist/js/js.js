5e6bb72d46977ae56f87b7fb178e4ad7120ec1f6
Francisco <franciscooliveira@gec.inatel.br>
2024-08-27 19:09:34 -0300

    Ex landpage filme

--- dist/js/js.js
+++ dist/js/js.js
@@ -1,10 +1,35 @@
 $(document).ready(function(){
-    $('.car').slick({
-      slidesToShow: 1, 
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 6000,
-      dots: false,
-      arrows: false
-    });
+  $('.car').slick({
+    slidesToShow: 1, 
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    dots: false,
+    arrows: false,
+    rows: 1
   });
+
+  $('.car1').slick({
+    slidesToShow: 4, 
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: false,
+    arrows: false, 
+    rows: 1,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 576, 
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
